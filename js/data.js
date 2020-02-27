c8c7dbd4f16cf33c5d86a0f8233a0d87ea28165d
leonidryb <leonidrybalkin@yandex.ru>
2020-02-27 23:32:18 +0300

    Исправляет замечания наставника

--- js/data.js
+++ js/data.js
@@ -1,38 +1,18 @@
 'use strict';
 
 window.data = (function () {
-  // var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  // var WIZARDS_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  // var WIZARDS_COUNT = 4;
-
-  // var createWizardsArray = function (countWizards) {
-  //   var array = [];
-  //   for (var i = 0; i < countWizards; i += 1) {
-  //     array.push(
-  //         {
-  //           name: window.util.getRandomElement(WIZARDS_NAMES) + ' ' + window.util.getRandomElement(WIZARDS_SECOND_NAMES),
-  //           coatColor: window.util.getRandomElement(COAT_COLORS),
-  //           eyesColor: window.util.getRandomElement(EYE_COLORS)
-  //         }
-  //     );
-  //   }
-  //   return array;
-  // };
-  //
-  // var wizards = createWizardsArray(WIZARDS_COUNT);
+  var WIZARDS_COUNT = 4;
 
   var userDialog = document.querySelector('.setup');
 
   var similarListElement = userDialog.querySelector('.setup-similar-list');
 
-  // userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
   var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content;
-    // .querySelector('.setup-similar-item');
 
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -46,7 +26,7 @@ window.data = (function () {
 
   var onSuccesLoadWizards = function (wizards) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
       fragment.appendChild(renderWizard(wizards[i]));
     }
     similarListElement.appendChild(fragment);
@@ -67,8 +47,6 @@ window.data = (function () {
   };
 
   window.backend.load(onSuccesLoadWizards, onError);
-
-  // createWizardsList();
 
   return {
     coatColors: COAT_COLORS,
